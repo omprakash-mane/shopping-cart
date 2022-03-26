ebc28b3a1cb1816ae0fb7508fdf7c7fb7200ace9
omprakash-mane <tech.om.projects@gmail.com>
2022-03-26 22:29:35 +0530

    Code changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import AppRouter from "./router";
 import { ProductOrderProvider } from "./context/ProductOrderProvider";
 import Header from "./components/Header";
-//hello 123
 function App() {
   return (
     <ProductOrderProvider>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,28 @@
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../pages/Home";
-import OrderConfirmation from "../pages/OrderConfirmation";
-import Products from "../pages/Products";
-import Checkout from "../pages/Checkout";
+import CircularIndeterminate from "../components/Loader";
+import "./styles.css";
+
+const Home = React.lazy(() => import("../pages/Home"));
+const OrderConfirmation = React.lazy(() =>
+  import("../pages/OrderConfirmation")
+);
+const Products = React.lazy(() => import("../pages/Products"));
+const Checkout = React.lazy(() => import("../pages/Checkout"));
+
 const AppRouter = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/order" component={OrderConfirmation} />
-        <Route path="/products" component={Products} />
-        <Route path="/checkout" component={Checkout} />
-      </Switch>
-    </Router>
+    <Suspense fallback={<CircularIndeterminate />} className="loader">
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/order" component={OrderConfirmation} />
+          <Route path="/products" component={Products} />
+          <Route path="/checkout" component={Checkout} />
+        </Switch>
+      </Router>
+    </Suspense>
   );
 };
 
